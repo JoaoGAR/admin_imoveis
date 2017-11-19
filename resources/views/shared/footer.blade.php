b414c022a09db47f58e3511b085e582b23edf730
Joao Gabriel <jgarchanjo@gmail.com>
2017-11-19 17:46:30 -0200

    Editar imovel OK

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-inverse navbar-fixed-bottom">
 	<div class="container">
 		<ul class="nav navbar-nav">
-			<li><a href="#">Link</a></li>
+			<li><a href="https://github.com/JoaoGAR" target="_blank()">JGAR - Githhub</a></li>
+			<li>Developed by JJGAR</li>
 		</ul>
 	</div>
 </nav>
